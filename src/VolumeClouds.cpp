47e33d2349ce0b90dff09c6b5b4cc1cf7ecd17b0
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2014-12-16 20:47:03 +0000

    yes

--- src/VolumeClouds.cpp
+++ src/VolumeClouds.cpp
@@ -302,6 +302,56 @@ void VolumetricClouds::Render(Graphics::Renderer *r, const Graphics::Frustum &fr
 	}
 }
 
+//Compat method: gluLookAt deprecated
+void util_compat_gluLookAt(GLfloat eyeX, GLfloat eyeY, GLfloat eyeZ, GLfloat lookAtX, GLfloat lookAtY, GLfloat lookAtZ, GLfloat upX, GLfloat upY, GLfloat upZ) {
+    vector3f x, y, z;
+    z = vector3f(eyeX-lookAtX, eyeY-lookAtY, eyeZ-lookAtZ).Normalized();
+    y = vector3f(upX, upY, upZ);
+    x = y.Cross(z);
+    y = z.Cross(x);
+    x = x.Normalized();
+    y = y.Normalized();
+    // mat is given transposed so OpenGL can handle it.
+    matrix4x4f mat (new GLfloat[16] {
+		x.x,  y.x,   z.x,   0,
+        x.y,  y.y,   z.y,   0,
+        x.z,  y.z,   z.z,   0,
+        -eyeX,     -eyeY,      -eyeZ,      1});
+    glMultMatrixf(mat.Data());
+}
+
+int glhProjectf(float objx, float objy, float objz, double *modelview, double *projection, int *viewport, double *wcx, double *wcy, double *wcz)
+  {
+      //Transformation vectors
+      float fTempo[8];
+      //Modelview transform
+      fTempo[0]=modelview[0]*objx+modelview[4]*objy+modelview[8]*objz+modelview[12];  //w is always 1
+      fTempo[1]=modelview[1]*objx+modelview[5]*objy+modelview[9]*objz+modelview[13];
+      fTempo[2]=modelview[2]*objx+modelview[6]*objy+modelview[10]*objz+modelview[14];
+      fTempo[3]=modelview[3]*objx+modelview[7]*objy+modelview[11]*objz+modelview[15];
+      //Projection transform, the final row of projection matrix is always [0 0 -1 0]
+      //so we optimize for that.
+      fTempo[4]=projection[0]*fTempo[0]+projection[4]*fTempo[1]+projection[8]*fTempo[2]+projection[12]*fTempo[3];
+      fTempo[5]=projection[1]*fTempo[0]+projection[5]*fTempo[1]+projection[9]*fTempo[2]+projection[13]*fTempo[3];
+      fTempo[6]=projection[2]*fTempo[0]+projection[6]*fTempo[1]+projection[10]*fTempo[2]+projection[14]*fTempo[3];
+      fTempo[7]=-fTempo[2];
+      //The result normalizes between -1 and 1
+      if(fTempo[7]==0.0)	//The w value
+         return 0;
+      fTempo[7]=1.0/fTempo[7];
+      //Perspective division
+      fTempo[4]*=fTempo[7];
+      fTempo[5]*=fTempo[7];
+      fTempo[6]*=fTempo[7];
+      //Window coordinates
+      //Map x, y to range 0-1
+      *wcx=(fTempo[4]*0.5+0.5)*viewport[2]+viewport[0];
+      *wcy=(fTempo[5]*0.5+0.5)*viewport[3]+viewport[1];
+      //This is only correct when glDepthRange(0.0, 1.0)
+      *wcz=(1.0+fTempo[6])*0.5;	//Between 0 and 1
+      return 1;
+  }
+
 void VolumetricClouds::MakeCloudImpostor(VolumetricCloud* Cloud, vector3f Sun, vector3f Camera)
 {
 	unsigned i;
@@ -335,7 +385,7 @@ void VolumetricClouds::MakeCloudImpostor(VolumetricCloud* Cloud, vector3f Sun, v
 	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
 	glLoadIdentity();
-	gluLookAt(Camera.x, Camera.y, Camera.z, Cloud->Center.x, Cloud->Center.y, Cloud->Center.z, 0, 1, 0);
+	util_compat_gluLookAt(Camera.x, Camera.y, Camera.z, Cloud->Center.x, Cloud->Center.y, Cloud->Center.z, 0, 1, 0);
 	
 	glPushAttrib(GL_VIEWPORT_BIT);
 	glViewport(0, 0, ViewportSize, ViewportSize);
@@ -497,7 +547,7 @@ void VolumetricClouds::LightCloud(VolumetricCloud* Cloud, vector3f Sun)
 	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
 	glLoadIdentity();		
-	gluLookAt(Sun.x, Sun.y, Sun.z, Cloud->Center.x, Cloud->Center.y, Cloud->Center.z, 0, 1, 0);
+	util_compat_gluLookAt(Sun.x, Sun.y, Sun.z, Cloud->Center.x, Cloud->Center.y, Cloud->Center.z, 0, 1, 0);
 		
 	glPushAttrib(GL_VIEWPORT_BIT);
 	glViewport(0, 0, SplatBufferSize, SplatBufferSize);
@@ -550,7 +600,7 @@ void VolumetricClouds::LightCloud(VolumetricCloud* Cloud, vector3f Sun)
 	{		
 		//find the particle's projected coordinates
 		double CenterX, CenterY, CenterZ;
-		gluProject(Cloud->Puffs[i].Position.x, 
+		glhProjectf(Cloud->Puffs[i].Position.x, 
 			Cloud->Puffs[i].Position.y, 
 			Cloud->Puffs[i].Position.z, 
 			mm, mp, vp, &CenterX, &CenterY, &CenterZ);
