392be0c9e6f3ec4876ece7bf3c1a156e508c7395
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2015-07-26 20:27:21 +0100

    New material for rendering using a floating point texture format - specifically for depth buffer textures.

---
+++ src/graphics/opengl/DepthTextureMaterial.cpp
@@ -0,0 +1,48 @@
+// Copyright © 2008-2015 Pioneer Developers. See AUTHORS.txt for details
+// Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+#include "DepthTextureMaterial.h"
+#include "graphics/Material.h"
+#include "graphics/Graphics.h"
+#include "TextureGL.h"
+#include "RendererGL.h"
+#include <sstream>
+#include "StringF.h"
+
+namespace Graphics {
+namespace OGL {
+
+DepthTextureProgram::DepthTextureProgram(const MaterialDescriptor &desc)
+{
+	m_name = "DepthTexture";
+	RendererOGL::CheckErrors();
+
+	LoadShaders(m_name, m_defines);
+	InitUniforms();
+}
+
+Program *DepthTextureMaterial::CreateProgram(const MaterialDescriptor &desc)
+{
+	return new DepthTextureProgram(desc);
+}
+
+void DepthTextureMaterial::Apply()
+{
+	OGL::Material::Apply();
+
+	DepthTextureProgram *p = static_cast<DepthTextureProgram*>(m_program);
+
+	p->texture0.Set(this->texture0, 0);
+	RendererOGL::CheckErrors();
+}
+
+void DepthTextureMaterial::Unapply()
+{
+	// Might not be necessary to unbind textures, but let's not old graphics code (eg, old-UI)
+	if (texture0) {
+		static_cast<TextureGL*>(texture0)->Unbind();
+	}
+}
+
+}
+}
