495bf838f7eea47e66f16bbbbeb514a7b8c10ede
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2024-10-23 20:23:10 +0100

    Initial setup of Vulkan renderer

--- src/pigui/PiGui.cpp
+++ src/pigui/PiGui.cpp
@@ -404,6 +404,9 @@ void Instance::Init(Graphics::Renderer *renderer)
 	case Graphics::RENDERER_OPENGL_3x:
 		m_instanceRenderer->Initialize();
 		break;
+	case Graphics::RENDERER_VULKAN:
+		m_instanceRenderer->Initialize();
+		break;
 	}
 
 	ImGuiIO &io = ImGui::GetIO();
@@ -481,6 +484,9 @@ void Instance::NewFrame()
 	case Graphics::RENDERER_OPENGL_3x:
 		m_instanceRenderer->NewFrame();
 		break;
+	case Graphics::RENDERER_VULKAN:
+		m_instanceRenderer->NewFrame();
+		break;
 	}
 	ImGui_ImplSDL2_NewFrame();
 	ImGui::NewFrame();
@@ -513,6 +519,9 @@ void Instance::Render()
 	case Graphics::RENDERER_DUMMY:
 		return;
 	case Graphics::RENDERER_OPENGL_3x:
+		m_instanceRenderer->RenderDrawData(ImGui::GetDrawData());
+		break;
+	case Graphics::RENDERER_VULKAN:
 		m_instanceRenderer->RenderDrawData(ImGui::GetDrawData());
 		break;
 	}
@@ -692,6 +701,9 @@ void Instance::Uninit()
 	case Graphics::RENDERER_DUMMY:
 		return;
 	case Graphics::RENDERER_OPENGL_3x:
+		m_instanceRenderer->Shutdown();
+		break;
+	case Graphics::RENDERER_VULKAN:
 		m_instanceRenderer->Shutdown();
 		break;
 	}
